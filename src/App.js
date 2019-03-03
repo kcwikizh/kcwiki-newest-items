47a1935047a87eb8385b2b425faa5863693082c3
lawvs <lawvs@users.noreply.github.com>
2019-03-04 02:55:54 +0800

    feat: add template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Item from './Item'
 
-function App() {
-  return <div />
+/**
+ * more update link
+ * @param {string} link
+ */
+function More({ link, text = '查看更多更新' }) {
+  return (
+    <span style={{ float: 'right' }} className="plainlinks">
+      [{link} {text}&gt;&gt;]
+    </span>
+  )
 }
 
+More.propTypes = {
+  link: PropTypes.string.isRequired,
+  text: PropTypes.string,
+}
+
+function NewestItems({ items, more }) {
+  if (!items) {
+    return (
+      <>
+        暂无重要更新
+        <More link={more} text={'查看其他更新'} />
+      </>
+    )
+  }
+
+  return (
+    <div
+      style={{
+        height: '390px',
+        padding: '0 10px',
+        overflow: 'auto',
+        overflowX: 'hidden',
+        overflowY: 'auto',
+      }}
+    >
+      <templatestyles src="最新词条/2/styles.css" />
+      <div>
+        <table width="100%">
+          {items.map((props, index) => (
+            <tr key={index}>
+              <td className="left" style={{ position: 'relative' }}>
+                <Item {...props} />
+              </td>
+            </tr>
+          ))}
+          <tr id="more-tr">
+            <td id="more-td">
+              <More link={more} />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+NewestItems.propTypes = {
+  items: PropTypes.array.isRequired,
+  more: PropTypes.string.isRequired,
+}
+
+function App(data) {
+  return <NewestItems {...data} />
+}
 export default App
---
+++ src/Item.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const photoStyle = {
+  position: 'absolute',
+  opacity: 0.5,
+  transform: ' rotateY(180deg)',
+  width: '100%',
+  pointerEvents: 'none',
+  top: 0,
+}
+
+function Item({ plainlink, remark, contributor, photo, date }) {
+  return (
+    <>
+      <p>
+        {'{{Label|danger|更新}}'}
+        <span className="plainlinks">[[{plainlink}]]</span>
+        {remark && <span className="beizhu">&nbsp;{remark}</span>}
+      </p>
+      {contributor && (
+        <p>
+          贡献者: {contributor}
+          {date && <span className="time">{date}</span>}
+        </p>
+      )}
+      <p style={photoStyle}>[[File:{photo}|350px]]</p>
+    </>
+  )
+}
+
+Item.propTypes = {
+  plainlink: PropTypes.string.isRequired,
+  remark: PropTypes.string,
+  contributor: PropTypes.string,
+  date: PropTypes.string,
+  photo: PropTypes.string,
+}
+
+export default Item
