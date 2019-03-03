23a93898f98785166b607e49eeca69321518c51e
lawvs <lawvs@users.noreply.github.com>
2019-03-04 02:56:35 +0800

    feat: update build

--- scripts/build.js
+++ scripts/build.js
@@ -4,15 +4,19 @@ const fs = require('fs')
 const React = require('react')
 // https://reactjs.org/docs/react-dom-server.html
 const ReactDOMServer = require('react-dom/server')
+const beautify = require('js-beautify').html
 const App = require('../src/index').default
+const data = require('../build/data.json')
 
-const appStr = ReactDOMServer.renderToStaticMarkup(React.createElement(App))
-
-console.log(appStr)
+const appStr = ReactDOMServer.renderToStaticMarkup(React.createElement(App, data))
 
 if (!fs.existsSync('dist')) {
   fs.mkdirSync('dist')
 }
 
 fs.writeFileSync('dist/index.js', appStr)
+
+const appStrFormat = beautify(appStr, { indent_size: 2 })
+fs.writeFileSync('dist/index.format.js', appStrFormat)
+
 console.log('Build Success!')
