550c4da4fb33398b8255bccf1b3c1bfacdd32969
lawvs <lawvs@users.noreply.github.com>
2019-03-05 05:15:14 +0800

    chore: resolve path

--- scripts/build.js
+++ scripts/build.js
@@ -5,8 +5,11 @@ const React = require('react')
 // https://reactjs.org/docs/react-dom-server.html
 const ReactDOMServer = require('react-dom/server')
 const beautify = require('js-beautify').html
-const App = require('../src/index').default
-const data = require('../build/data.json')
+const utils = require('./utils')
+const config = require('./config')
+const App = require(utils.resolvePath('src/index')).default
+const items = require(utils.resolvePath('build/data.json'))
+const data = { items, more: config.moreLink }
 
 const appStr = ReactDOMServer.renderToStaticMarkup(React.createElement(App, data))
 
