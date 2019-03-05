a2f418fd199b967caaa0d7522f762d7b040fecde
lawvs <lawvs@users.noreply.github.com>
2019-03-05 15:20:05 +0800

    chore: change build dir

--- scripts/build.js
+++ scripts/build.js
@@ -5,21 +5,21 @@ const React = require('react')
 // https://reactjs.org/docs/react-dom-server.html
 const ReactDOMServer = require('react-dom/server')
 const beautify = require('js-beautify').html
-const utils = require('./utils')
+const { resolvePath } = require('./utils')
 const config = require('./config')
-const App = require(utils.resolvePath('src/index')).default
-const items = require(utils.resolvePath('build/data.json'))
+const App = require(resolvePath('src/index')).default
+const items = require(resolvePath('build/data.json'))
 const data = { items, more: config.moreLink }
 
 const appStr = ReactDOMServer.renderToStaticMarkup(React.createElement(App, data))
 
-if (!fs.existsSync('dist')) {
-  fs.mkdirSync('dist')
+if (!fs.existsSync('build')) {
+  fs.mkdirSync('build')
 }
 
-fs.writeFileSync('dist/index.html', appStr)
+fs.writeFileSync('build/index.html', appStr)
 
 const appStrFormat = beautify(appStr, { indent_size: 2 })
-fs.writeFileSync('dist/index.formatted.html', appStrFormat)
+fs.writeFileSync('build/index.formatted.html', appStrFormat)
 
 console.log('Build Success!')
