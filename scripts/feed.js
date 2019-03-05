db7dbc8ca63c8c172c2ca7b2d97d5d9d5bdec9ce
lawvs <lawvs@users.noreply.github.com>
2019-03-06 01:50:54 +0800

    refactor: feed

--- scripts/feed.js
+++ scripts/feed.js
@@ -1,76 +1,48 @@
 // https://github.com/danmactough/node-feedparser
 const fs = require('fs')
-const FeedParser = require('feedparser')
-const fetch = require('node-fetch')
 const config = require('./config')
-const { resolvePath } = require('./utils')
+const { resolvePath, parseFeed } = require('./utils')
 
 const { LOCAL } = process.env
-const feedparser = new FeedParser()
-
-if (!fs.existsSync('build')) {
-  fs.mkdirSync('build')
-}
-
-if (LOCAL) {
-  console.log('Using Local Mode.')
-  if (!fs.existsSync('build/feed.json')) {
-    console.error("ERROR! file 'build/feed.json' not exist")
-    process.exit(1)
-  }
-  const items = require(resolvePath('build/feed.json')).map(({ date, ...rest }) => ({
-    date: new Date(date), // parse date
-    ...rest,
-  }))
-  const dataStr = JSON.stringify(parseData(items), undefined, 2)
-  fs.writeFileSync('build/data.json', dataStr)
-  console.log('Feed Success!')
-  process.exit(0)
-}
-
-console.log(`Fetching ${config.feedLink}`)
-
-const items = []
-fetch(config.feedLink)
-  .then(res => res.body)
-  .then(stream => stream.pipe(feedparser))
-  .catch(err => console.error(err))
-
-feedparser.on('error', function(err) {
-  console.error(err)
-  process.exit(1)
-})
-
-feedparser.on('readable', function() {
-  const stream = this // `this` is `feedparser`, which is a stream
-  // const meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-  let item
-  while ((item = stream.read())) {
-    items.push(item)
-  }
-})
-
-feedparser.on('end', function() {
-  const feedStr = JSON.stringify(items, undefined, 2)
-  fs.writeFileSync('build/feed.json', feedStr)
-  const dataStr = JSON.stringify(parseData(items), undefined, 2)
-  fs.writeFileSync('build/data.json', dataStr)
-  console.log('Feed Success!')
-})
 
 /**
  * @param {import('FeedParser').Item[]} items
  */
 function parseData(items) {
-  return items.map(({ title, description, date, author }) => {
-    const regex = /(?<=^<p>‎<span dir="auto"><span class="autocomment">).*?(?=<\/span><\/span><\/p>)/.exec(
-      description,
-    )
-    return {
-      plainlink: title,
-      remark: regex && regex[0].length <= 10 ? regex[0] : '',
-      date: date.toLocaleDateString(),
-      contributor: author,
-    }
-  })
+  return items
+    .map(({ title, description, date, author }) => {
+      const regex = /(?<=^<p>‎<span dir="auto"><span class="autocomment">).*?(?=<\/span><\/span><\/p>)/.exec(
+        description,
+      )
+      return {
+        plainlink: title,
+        remark: regex ? regex[0] : undefined,
+        date: date.toLocaleDateString(),
+        contributor: author,
+      }
+    })
+    .filter(({ remark }) => remark && remark.length <= 10)
 }
+
+async function main() {
+  if (!fs.existsSync('build')) {
+    fs.mkdirSync('build')
+  }
+  let items
+  if (LOCAL) {
+    console.log('Using Local Mode.')
+    items = require(resolvePath('build/feed.json')).map(({ date, ...rest }) => ({
+      date: new Date(date), // parse date
+      ...rest,
+    }))
+  } else {
+    console.log(`Fetching ${config.feedLink}`)
+    items = await parseFeed(config.feedLink)
+    fs.writeFileSync('build/feed.json', JSON.stringify(items, undefined, 2))
+  }
+
+  fs.writeFileSync('build/data.json', JSON.stringify(parseData(items), undefined, 2))
+  console.log('Feed Success!')
+}
+
+main()
--- scripts/utils.js
+++ scripts/utils.js
@@ -3,3 +3,31 @@ const path = require('path')
 exports.resolvePath = function(dir = '') {
   return path.join(__dirname, '..', dir)
 }
+
+exports.parseFeed = function(url) {
+  const fetch = require('node-fetch')
+  const FeedParser = require('feedparser')
+  const feedparser = new FeedParser()
+  return fetch(url)
+    .then(res => res.body)
+    .then(stream => stream.pipe(feedparser))
+    .then(
+      () =>
+        new Promise((resolve, reject) => {
+          const items = []
+          feedparser.on('readable', function() {
+            const stream = this
+            let item
+            while ((item = stream.read())) {
+              items.push(item)
+            }
+          })
+          feedparser.on('error', err => {
+            reject(err)
+          })
+          feedparser.on('end', () => {
+            resolve(items)
+          })
+        }),
+    )
+}
