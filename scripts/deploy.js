57a8c579629fce7e33e6f85cd1bb21aa610b947c
lawvs <lawvs@users.noreply.github.com>
2019-03-07 17:53:19 +0800

    chore: optimize log output

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,23 +7,27 @@ const bot = new Bot(config.bot)
 const content = fs.readFileSync('build/index.html', { encoding: 'utf-8' })
 
 if (!content) {
-  console.error('Not Content! Deploy ERROR!')
+  console.error("Deploy ERROR! Empty deploy content! Please run 'npm run feed' first. ")
   process.exit(1)
 }
 
-const { deployTitle: title, summary } = config
-// bot.getArticle(title, (err, data) => console.log(err, data))
-const minor = true
+const { username, password } = config.bot
+if (!username && !password) {
+  console.log('Deploy ERROR! No username or password!')
+  process.exit(1)
+}
 
 console.log('Logging...')
-bot.logIn(config.bot.username, config.bot.password, err => {
+bot.logIn(username, password, err => {
   if (err) {
     console.error('Login ERROR!', err)
     process.exit(1)
     return
   }
   console.log('Deploying...')
-
+  const { deployTitle: title, summary } = config
+  // bot.getArticle(title, (err, data) => console.log(err, data))
+  const minor = true
   bot.edit(title, content, summary, minor, (err, data) => {
     if (err) {
       console.error(err)
