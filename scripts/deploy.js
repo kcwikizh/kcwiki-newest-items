04036ddbea3b437850e8674deb35166530013913
lawvs <lawvs@users.noreply.github.com>
2019-03-07 17:06:17 +0800

    fix: login

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,6 @@ const Bot = require('nodemw')
 const config = require('./config')
 
 const bot = new Bot(config.bot)
-
 const content = fs.readFileSync('build/index.html', { encoding: 'utf-8' })
 
 if (!content) {
@@ -15,12 +14,24 @@ if (!content) {
 const { deployTitle: title, summary } = config
 // bot.getArticle(title, (err, data) => console.log(err, data))
 const minor = true
-bot.edit(title, content, summary, minor, (err, data) => {
+
+console.log('Logging...')
+bot.logIn(config.bot.username, config.bot.password, err => {
   if (err) {
-    console.error(err)
-    console.log('Deploy ERROR!')
+    console.error('Login ERROR!', err)
+    process.exit(1)
     return
   }
-  console.log(data)
-  console.log('Deploy Success!')
+  console.log('Deploying...')
+
+  bot.edit(title, content, summary, minor, (err, data) => {
+    if (err) {
+      console.error(err)
+      console.log('Deploy ERROR!')
+      process.exitCode = 1
+      return
+    }
+    console.log('Deploy Result: ', data)
+    console.log('Deploy Finish!')
+  })
 })
